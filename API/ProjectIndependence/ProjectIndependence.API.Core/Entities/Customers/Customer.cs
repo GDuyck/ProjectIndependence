01d69458b198b2d8e65360b33eeab2630fe27281
Lobsie <gijs.duyck@gmail.com>
2025-07-09 11:04:52 +0200

    Create customer and quotation entities

---
+++ API/ProjectIndependence/ProjectIndependence.API.Core/Entities/Customers/Customer.cs
@@ -0,0 +1,17 @@
+﻿using ProjectIndependence.API.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectIndependence.API.Core.Entities.Customers
+{
+    public class Customer : EntityBase
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string BillingAddress { get; set; }
+        public string ShippingAddress { get; set; }
+    }
+}
--- API/ProjectIndependence/ProjectIndependence.API/obj/Debug/net8.0/ProjectIndependence.API.AssemblyInfo.cs
+++ API/ProjectIndependence/ProjectIndependence.API/obj/Debug/net8.0/ProjectIndependence.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectIndependence.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eaa19766f8d34542fd764e5f27e1b47b8accb8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectIndependence.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectIndependence.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
