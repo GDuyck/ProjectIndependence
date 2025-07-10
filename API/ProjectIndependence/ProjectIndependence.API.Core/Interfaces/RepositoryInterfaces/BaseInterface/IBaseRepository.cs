46f7c6a259e1c0cf6e358b6c5dec6d337fdb2d6b
Lobsie <gijs.duyck@gmail.com>
2025-07-10 11:16:41 +0200

    Add ibaserepository

---
+++ API/ProjectIndependence/ProjectIndependence.API.Core/Interfaces/RepositoryInterfaces/BaseInterface/IBaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using ProjectIndependence.API.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectIndependence.API.Core.Interfaces.RepositoryInterfaces.BaseInterface
+{
+    internal interface IBaseRepository<TEntity> 
+        where TEntity : EntityBase
+    {
+        Task<TEntity> GetByIdAsync(Guid id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> AddAsync(TEntity entity);
+        Task<TEntity> UpdateAsync(TEntity entity);
+        Task DeleteAsync(Guid id);
+    }
+}
