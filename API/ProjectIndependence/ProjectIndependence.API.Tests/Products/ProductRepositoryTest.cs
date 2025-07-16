b456cd2eb011482fe4d922e162c63635e7b5efd1
Lobsie <gijs.duyck@gmail.com>
2025-07-16 13:08:22 +0200

    Add static method to create mock datasets

---
+++ API/ProjectIndependence/ProjectIndependence.API.Tests/Products/ProductRepositoryTest.cs
@@ -0,0 +1,42 @@
+﻿using Moq;
+using ProjectIndependence.API.Core.Entities.Products;
+using ProjectIndependence.API.Core.Interfaces.RepositoryInterfaces.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectIndependence.API.Tests.Products
+{
+    public class ProductRepositoryTest
+    {
+        private readonly Mock<IProductRepository> _productRepositoryMock;
+        private readonly List<Product> _mockProducts;
+
+        public ProductRepositoryTest()
+        {
+            _mockProducts = new List<Product>
+            {
+                new Product
+                {
+                    Name = "Test product 1",
+                    Price = 20,
+                    Tax = 21
+                },
+                new Product
+                {
+                    Name = "Test product 2",
+                    Price = 40,
+                    Tax = 12
+                }
+            };
+
+            _productRepositoryMock = new Mock<IProductRepository>();
+
+            _productRepositoryMock.Setup(repo => repo.GetAllAsync()).Returns(() =>  _mockProducts);
+
+
+        }
+    }
+}
---
+++ API/ProjectIndependence/ProjectIndependence.API.Tests/Static/CreateMockSet.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using ProjectIndependence.API.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectIndependence.API.Tests.Static
+{
+    public static class CreateMockSet
+    {
+        public static Mock<DbSet<T>> CreateMockDbSet<T>(IEnumerable<T> mockData) where T : EntityBase
+        {
+            var queryableData = mockData.AsQueryable();
+
+            var mockSet = new Mock<DbSet<T>>();
+
+            mockSet.As<IQueryable<T>>()
+                .Setup(m => m.Provider)
+                .Returns(queryableData.Provider);
+
+            mockSet.As<IQueryable<T>>()
+                .Setup(m => m.Expression)
+                .Returns(queryableData.Expression);
+
+            mockSet.As<IQueryable<T>>()
+                .Setup(m => m.ElementType)
+                .Returns(queryableData.ElementType);
+
+            mockSet.As<IQueryable<T>>()
+                .Setup(m => m.GetEnumerator())
+                .Returns(queryableData.GetEnumerator());
+
+            return mockSet;
+        }
+    }
+}
