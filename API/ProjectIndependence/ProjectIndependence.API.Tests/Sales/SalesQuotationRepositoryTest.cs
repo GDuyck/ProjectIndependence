645e3966278b1739241cc9667597d0125c7561f0
Lobsie <gijs.duyck@gmail.com>
2025-07-23 13:23:33 +0200

    Add tests for salesquotation repo

--- API/ProjectIndependence/ProjectIndependence.API.Tests/Sales/SalesQuotationRepositoryTest.cs
+++ API/ProjectIndependence/ProjectIndependence.API.Tests/Sales/SalesQuotationRepositoryTest.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using ProjectIndependence.API.Core.Entities.Sales;
 using ProjectIndependence.API.Core.Interfaces.RepositoryInterfaces.Sales;
+using ProjectIndependence.API.Infrastructure.Repositories.Sales;
 using ProjectIndependence.API.Tests.Servicebuilder;
 using System;
 using System.Collections.Generic;
@@ -15,10 +16,10 @@ namespace ProjectIndependence.API.Tests.Sales
     {
         private readonly ISalesQuotationRepository salesQuotationRepository;
         private readonly ServiceProvider serviceProvider;
-        private readonly List<SalesQuotation> mockSalesQuotation;
+        private readonly List<SalesQuotation> mockSalesQuotations;
         public SalesQuotationRepositoryTest()
         {
-            mockSalesQuotation = new List<SalesQuotation>
+            mockSalesQuotations = new List<SalesQuotation>
             {
                 new SalesQuotation
                 {
@@ -36,9 +37,112 @@ namespace ProjectIndependence.API.Tests.Sales
                 }
             };
 
-            serviceProvider = CreateServiceProvider.CreateProvider(mockSalesQuotation);
+            serviceProvider = CreateServiceProvider.CreateProvider(mockSalesQuotations);
 
             salesQuotationRepository = serviceProvider.GetRequiredService<ISalesQuotationRepository>();
+        }
+
+        [Fact]
+        public async Task SalesQuotationRepository_GetAllAsync_ReturnsAllQuotationsAndIsNotNull()
+        {
+            // ACT
+            var result = await salesQuotationRepository.GetAllAsync();
+
+            // ASSERT
+            Assert.NotNull(result);
+            Assert.Equal(mockSalesQuotations.Count, result.Count());
+        }
+
+        [Fact]
+        public async Task SalesQuotationRepository_GetById_ReturnsQuotationWithAValidIdAsync()
+        {
+            // ARRANGE
+            var testId = mockSalesQuotations[0].Id;
+
+            // ACT
+            var getByIdResult = await salesQuotationRepository.GetByIdAsync(testId);
+
+            // ASSERT
+            Assert.NotNull(getByIdResult);
+            Assert.Equal(testId, getByIdResult.Id);
+        }
+
+        [Fact]
+        public async Task SalesQuotationRepository_GetById_ReturnsNullWithInvalidId()
+        {
+            // ARRANGE
+            var testId = Guid.Parse("1134c810-922a-47e2-90d1-ae0ed129aaab");
+
+            // ACT
+            var result = await salesQuotationRepository.GetByIdAsync(testId);
+
+            // ASSERT
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task SalesQuotationRepository_AddAsync_AddsQuotationtAndReturnsTheAddedQuotation()
+        {
+            // ARRANGE
+            var newSalesQuotation = new SalesQuotation
+            {
+                Id = new Guid(),
+                TotalPrice = 148795,
+                Status = "ok"
+            };
+
+            // ACT
+            var result = await salesQuotationRepository.AddAsync(newSalesQuotation);
+
+            // ASSERT
+            Assert.Equal(newSalesQuotation.TotalPrice, result.TotalPrice);
+            Assert.Equal(newSalesQuotation.Id, result.Id);
+        }
+
+        [Fact]
+        public async Task SalesQuotationRepository_UpdateAsync_ReturnQuotationWithUpdatedValues()
+        {
+            // ARRANGE
+            decimal newTotal = 1487;
+
+            // ACT
+            var quotationToUpdate = await salesQuotationRepository.GetByIdAsync(mockSalesQuotations[0].Id);
+
+            quotationToUpdate.TotalPrice = newTotal;
+
+            var result = await salesQuotationRepository.UpdateAsync(quotationToUpdate);
+
+            // ASSERT
+            Assert.Equal(newTotal, result.TotalPrice);
+            Assert.Equal(quotationToUpdate.Id, result.Id);
+        }
+
+        [Fact]
+        public async Task SalesQuotationRepository_DeleteAsync_ReturnsTrueIfQuotationExistsAndGetsDeleted()
+        {
+            // ARRANGE
+            var testId = mockSalesQuotations[0].Id;
+
+            // ACT
+            var result = await salesQuotationRepository.DeleteAsync(testId);
+            var notExistingQuotation = await salesQuotationRepository.GetByIdAsync(testId);
+
+            // ASSERT
+            Assert.True(result);
+            Assert.Null(notExistingQuotation);
+        }
+
+        [Fact]
+        public async Task SalesQuotationRepository_DeleteAsync_ReturnsFalseIfQuotationDoesNotExist()
+        {
+            // ARRANGE
+            var testId = Guid.Parse("1134c810-922a-47e2-90d1-ae0ed12901aa");
+
+            // ACT
+            var result = await salesQuotationRepository.DeleteAsync(testId);
+
+            // ASSERT
+            Assert.False(result);
         }
     }
 }
