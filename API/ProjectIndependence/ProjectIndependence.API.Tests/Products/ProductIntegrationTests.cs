1d82aafd72b39354692a461252d8e8f9c3d2858a
Lobsie <gijs.duyck@gmail.com>
2025-08-27 13:16:40 +0200

    Add fluentassertion for testing and add test for getbyid

--- API/ProjectIndependence/ProjectIndependence.API.Tests/Products/ProductIntegrationTests.cs
+++ API/ProjectIndependence/ProjectIndependence.API.Tests/Products/ProductIntegrationTests.cs
@@ -1,6 +1,8 @@
-﻿using ProjectIndependence.API.Core.Dtos.Products;
+﻿using FluentAssertions;
+using ProjectIndependence.API.Core.Dtos.Products;
 using ProjectIndependence.API.Core.Response;
 using ProjectIndependence.API.Tests.Integration;
+using ProjectIndependence.API.Tests.Seeding;
 using System.Net.Http.Json;
 using Xunit;
 
@@ -24,9 +26,29 @@ namespace ProjectIndependence.API.Tests.Products
             var result = await response.Content.ReadFromJsonAsync<ApiResponse<DtoProduct[]>>();
 
             // ASSERT
-            Assert.NotNull(result);
-            Assert.True(result.Success);
-            Assert.NotNull(result.Data);
+            response.Should().NotBeNull();
+            result.Success.Should().BeTrue();
+            result.Data.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task GetById_ReturnsOkWithProduct()
+        {
+            // ARRANGE
+            var productToLookUp = SeedingData.ProductsToSeed()[0]; // First product in the list
+
+            // ACT
+            var response = await _httpClient.GetAsync($"api/products/{productToLookUp.Id}");
+            response.EnsureSuccessStatusCode();
+
+            var result = await response.Content.ReadFromJsonAsync<ApiResponse<DtoProduct>>();
+
+            // ASSERT
+            response.Should().NotBeNull();
+            result.Success.Should().BeTrue();
+            result.Data.Should().NotBeNull();
+            result.Data.Id.Should().Be(productToLookUp.Id);
+            result.Data.Name.Should().Be(productToLookUp.Name);
         }
     }
 }
---
+++ API/ProjectIndependence/ProjectIndependence.API.Tests/Seeding/SeedingData.cs
@@ -0,0 +1,33 @@
+﻿using ProjectIndependence.API.Core.Entities.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectIndependence.API.Tests.Seeding
+{
+    public static class SeedingData
+    {
+        public static List<Product> ProductsToSeed()
+        {
+            return new List<Product>
+            {
+                new Product
+                        {
+                            Id = Guid.Parse("9ee738a9-2d29-44b0-8d3a-92c8b4f0f622"),
+                            Name = "Test product 1",
+                            Price = 20,
+                            Tax = 21,
+                        },
+                        new Product
+                        {
+                            Id = Guid.Parse("1134c810-922a-47e2-90d1-ae0ed12901aa"),
+                            Name = "Test product 2",
+                            Price = 40,
+                            Tax = 12
+                        }
+            };
+        }
+    }
+}
