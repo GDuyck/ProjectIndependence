2226f62dbc98f6300ff7fe5a763f6ccc0c98d170
Lobsie <gijs.duyck@gmail.com>
2025-09-04 12:31:04 +0200

    Add new properties to product entity

--- API/ProjectIndependence/ProjectIndependence.API.Core/Entities/Products/Product.cs
+++ API/ProjectIndependence/ProjectIndependence.API.Core/Entities/Products/Product.cs
@@ -5,18 +5,53 @@ namespace ProjectIndependence.API.Core.Entities.Products
 {
     public class Product : EntityBase
     {
-        public string Name { get; set; }
-        public decimal Price { get; set; }
+        // Core info
+        public string Name { get; private set; }
+        public string ProductCode { get; private set; }
+        public string Description { get; private set; }
+        public bool IsActive { get; private set; }
+
+        // Classification (can be added later)
+
+        // Pricing
+        public decimal RetailPrice { get; set; }
+        public decimal CostPrice { get; set; }
         public TaxRate Tax { get; internal set; }
+
+        // Inventory
+        public int Stock { get; set; }
+
+        // Auditing
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public string CreatedBy { get; set; } // Placeholder, will be updated with person later
 
         private Product() { }
 
-        public Product(Guid id, string name, decimal price, int tax)
+        public Product(Guid id, 
+            string name, 
+            string productCode, 
+            string description, 
+            bool isActive, 
+            decimal retailPrice, 
+            decimal costPrice, 
+            int tax,
+            int stock,
+            string createdBy)
         {
             Id = id;
             Name = name;
-            Price = price;
+            ProductCode = productCode;
+            Description = description;
+            IsActive = isActive;
+            RetailPrice = retailPrice;
+            CostPrice = costPrice;
             Tax = new TaxRate(tax);
+            Stock = stock;
+            CreatedAt = DateTime.Now;
+            UpdatedAt = DateTime.Now;
+            CreatedBy = createdBy;
         }
     }
 }
