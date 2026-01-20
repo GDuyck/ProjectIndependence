e8d2a1a7d7c21faef757b4d147e6491fb2738bbd
Lobsie <gijs.duyck@gmail.com>
2026-01-20 16:10:52 +0100

    Add seeding data for unit and integration tests

--- API/ProjectIndependence/ProjectIndependence.API.Tests/Seeding/SeedingData.cs
+++ API/ProjectIndependence/ProjectIndependence.API.Tests/Seeding/SeedingData.cs
@@ -1,21 +1,64 @@
-﻿//using ProjectIndependence.API.Core.Entities.Products;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using ProjectIndependence.API.Core.Entities.Products;
 
-//namespace ProjectIndependence.API.Tests.Seeding
-//{
-//    public static class SeedingData
-//    {
-//        public static List<Product> ProductsToSeed()
-//        {
-//            return new List<Product>
-//            {
-//                new Product(Guid.Parse("9ee738a9-2d29-44b0-8d3a-92c8b4f0f622"), "Test product 1",20,21),
-//                new Product(Guid.Parse("1134c810-922a-47e2-90d1-ae0ed12901aa"),"Test product 2",40,12)
-//            };
-//        }
-//    }
-//}
+namespace ProjectIndependence.API.Tests.Seeding
+{
+    public static class SeedingData
+    {
+        public static IEnumerable<Product> ProductsToSeed()
+        {
+            return new List<Product>
+            {
+                new Product(
+                    Guid.Parse("11111111-1111-1111-1111-111111111111"),
+                    "Seed Product A",
+                    "SPA-001",
+                    "First seeded product",
+                    true,
+                    19.99m,
+                    10.00m,
+                    21,
+                    100,
+                    "Seeder"),
+
+                new Product(
+                    Guid.Parse("22222222-2222-2222-2222-222222222222"),
+                    "Seed Product B",
+                    "SPB-002",
+                    "Second seeded product",
+                    true,
+                    49.50m,
+                    25.00m,
+                    21,
+                    50,
+                    "Seeder"),
+
+                new Product(
+                    Guid.Parse("33333333-3333-3333-3333-333333333333"),
+                    "Seed Product C",
+                    "SPC-003",
+                    "Third seeded product",
+                    false,
+                    9.99m,
+                    4.00m,
+                    6,
+                    0,
+                    "Seeder")
+            };
+        }
+
+        public static Product SingleProduct()
+        {
+            return new Product(
+                Guid.Parse("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"),
+                "Single Seed",
+                "SS-100",
+                "Single seeded product",
+                true,
+                5.00m,
+                2.50m,
+                10,
+                5,
+                "Seeder");
+        }
+    }
+}
