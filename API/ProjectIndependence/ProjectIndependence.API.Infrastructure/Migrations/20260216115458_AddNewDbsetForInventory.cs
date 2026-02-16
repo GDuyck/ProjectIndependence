8c37e9c4d5c1eb3b16f5f7c79fb851d8064d02e7
Lobsie <gijs.duyck@gmail.com>
2026-02-16 13:06:43 +0100

    Add unittestclass for inventoryentity #15

---
+++ API/ProjectIndependence/ProjectIndependence.API.Infrastructure/Migrations/20260216115458_AddNewDbsetForInventory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectIndependence.API.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewDbsetForInventory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Inventories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    QuantityOnHand = table.Column<int>(type: "int", nullable: false),
+                    QuantityReserved = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inventories", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Inventories");
+        }
+    }
+}
---
+++ API/ProjectIndependence/ProjectIndependence.API.Tests/Inventory/UnitTests/EntityTests/InventroyEntityTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectIndependence.API.Tests.Inventory.UnitTests.EntityTests
+{
+    public class InventroyEntityTest
+    {
+    }
+}
