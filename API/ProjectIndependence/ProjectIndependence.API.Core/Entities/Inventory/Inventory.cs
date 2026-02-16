8577d4f65bc9ead3262c1f69765088507feef3e8
Lobsie <gijs.duyck@gmail.com>
2026-02-16 12:48:06 +0100

    Create new inventory entitiy #15

---
+++ API/ProjectIndependence/ProjectIndependence.API.Core/Entities/Inventory/Inventory.cs
@@ -0,0 +1,18 @@
+﻿using ProjectIndependence.API.Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ProjectIndependence.API.Core.Entities.Inventory
+{
+    public class Inventory : EntityBase
+    {
+        public Guid ProductId { get; set; }
+        public int QuantityOnHand { get; set; }
+        public int QuantityReserved { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
