711969a80ecfd60d4e99cb10ec14d3bbfe489a85
Lobsie <gijs.duyck@gmail.com>
2026-02-18 16:15:26 +0100

    Implement iinventory interface in inventoryrepository #17

---
+++ API/ProjectIndependence/ProjectIndependence.API.Infrastructure/Repositories/InventoryRepository.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectIndependence.API.Application.Interfaces.Inventories;
+using ProjectIndependence.API.Core.Entities.Inventories;
+using ProjectIndependence.API.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectIndependence.API.Infrastructure.Repositories
+{
+    public class InventoryRepository : IInventoryRepository
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public InventoryRepository(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<Inventory> GetInventoryByProductIdAsync(Guid productId, CancellationToken cancellationToken = default)
+        {
+            var inventory = await _applicationDbContext.Inventories
+                .FirstOrDefaultAsync(i => i.ProductId == productId, cancellationToken);
+
+            return inventory;
+        }
+
+        public async Task<Inventory> GetInventoryByProductIdReadOnlyAsync(Guid productId, CancellationToken cancellationToken = default)
+        {
+            var inventory = await _applicationDbContext.Inventories
+                .AsNoTracking()
+                .FirstOrDefaultAsync(i => i.ProductId == productId, cancellationToken);
+
+            return inventory;
+        }
+
+        public async Task<IReadOnlyList<Inventory>> InventoryListAsync(CancellationToken cancellationToken = default)
+        {
+            var inventoryList = await _applicationDbContext.Inventories
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+
+            return inventoryList;
+        }
+        public async Task<Inventory> CreateInventoryAsync(Inventory inventory, CancellationToken cancellationToken = default)
+        {
+            var createdInventory = await _applicationDbContext.Inventories
+                .AddAsync(inventory, cancellationToken);
+
+            return createdInventory.Entity;
+        }
+
+        public async Task<Inventory> UpdateInventoryAsync(Inventory inventory, CancellationToken cancellationToken = default)
+        {
+            var updatedInventory = await Task.FromResult(_applicationDbContext.Inventories.Update(inventory));
+
+            return updatedInventory.Entity;
+        }
+    }
+}
