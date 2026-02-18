b6e8ba7dfaaeb52b88b7fb386d6b3942641e36f0
Lobsie <gijs.duyck@gmail.com>
2026-02-18 09:20:21 +0100

    Add unit of work and iproductrepository #18 #17

---
+++ API/ProjectIndependence/ProjectIndependence.API.Application/Interfaces/Products/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using ProjectIndependence.API.Core.Entities.Products;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectIndependence.API.Application.Interfaces.Products
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProductByIdAsync(Guid id, CancellationToken cancellationToken);
+        Task<IReadOnlyList<Product>> GetProductListAsync(CancellationToken cancellationToken);
+        Task<Product> CreateProductAsync(Product product, CancellationToken cancellationToken);
+        Task<Product> UpdateProductAsync(Product product, CancellationToken cancellationToken);
+    }
+}
