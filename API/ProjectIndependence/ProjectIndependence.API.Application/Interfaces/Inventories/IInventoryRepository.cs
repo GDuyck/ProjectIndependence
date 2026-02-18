866c4869d5efb1c9317906badcfc856d38229aec
Lobsie <gijs.duyck@gmail.com>
2026-02-18 16:06:48 +0100

    Add interfaces for inventoryrepository and inventorymovementrepository #17 #18

---
+++ API/ProjectIndependence/ProjectIndependence.API.Application/Interfaces/Inventories/IInventoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using ProjectIndependence.API.Core.Entities.Inventories;
+
+namespace ProjectIndependence.API.Application.Interfaces.Inventories
+{
+    public interface IInventoryRepository
+    {
+        Task<Inventory> GetInventoryByProductIdAsync(Guid productId, CancellationToken cancellationToken = default);
+
+        Task<Inventory> GetInventoryByProductIdReadOnlyAsync(Guid productId, CancellationToken cancellationToken = default);
+
+        Task<Inventory> CreateInventoryAsync(Inventory inventory, CancellationToken cancellationToken = default);
+
+        Task<Inventory> UpdateInventoryAsync(Inventory inventory, CancellationToken cancellationToken = default);
+
+        Task<IReadOnlyList<Inventory>> InventoryListAsync(CancellationToken cancellationToken = default);
+    }
+}
