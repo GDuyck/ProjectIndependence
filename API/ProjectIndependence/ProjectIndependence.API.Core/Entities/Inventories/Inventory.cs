bbbe7698c07f57e9fff44ace072300a75bdffe92
Lobsie <gijs.duyck@gmail.com>
2026-02-18 08:50:58 +0100

    Add inventorymovement to inventory #18

--- API/ProjectIndependence/ProjectIndependence.API.Core/Entities/Inventories/Inventory.cs
+++ API/ProjectIndependence/ProjectIndependence.API.Core/Entities/Inventories/Inventory.cs
@@ -1,4 +1,5 @@
 ﻿using ProjectIndependence.API.Core.Entities.Base;
+using ProjectIndependence.API.Core.Enums;
 using ProjectIndependence.API.Core.Errors;
 using ProjectIndependence.API.Core.Exceptions;
 
@@ -6,6 +7,8 @@ namespace ProjectIndependence.API.Core.Entities.Inventories
 {
     public class Inventory : EntityBase
     {
+        private readonly List<InventoryMovement> _movements = new();
+
         public Guid ProductId { get; private set; }
         public int QuantityOnHand { get; private set; }
         public int QuantityReserved { get; private set; }
@@ -13,6 +16,8 @@ namespace ProjectIndependence.API.Core.Entities.Inventories
         public DateTime UpdatedAt { get; private set; }
         public string CreatedBy { get; private set; }
         public int AvailableStock => QuantityOnHand - QuantityReserved;
+
+        public IReadOnlyCollection<InventoryMovement> Movements => _movements;
 
         public Inventory(Guid productId, int quantityOnHand, string createdBy)
         {
@@ -27,6 +32,8 @@ namespace ProjectIndependence.API.Core.Entities.Inventories
         {
             EnsurePositiveQuantity(quantity);
 
+            AddMovement(quantity, InventoryMovementType.Purchase, "Stock increase");
+
             QuantityOnHand += quantity;
             UpdatedAt = DateTime.Now;
         }
@@ -37,6 +44,8 @@ namespace ProjectIndependence.API.Core.Entities.Inventories
 
             if (quantity > AvailableStock)
                 throw new InventoryException(InventoryErrors.QuantityExceedsAvailable);
+
+            AddMovement(-quantity, InventoryMovementType.Sale, "Stock Decrease");
 
             QuantityOnHand -= quantity;
             UpdatedAt = DateTime.Now;
@@ -49,6 +58,8 @@ namespace ProjectIndependence.API.Core.Entities.Inventories
             if (quantity > AvailableStock)
                 throw new InventoryException(InventoryErrors.QuantityExceedsAvailable);
 
+            AddMovement(-quantity, InventoryMovementType.Reservation, "Stock reservation");
+
             QuantityReserved += quantity;
             UpdatedAt = DateTime.Now;
         }
@@ -60,6 +71,8 @@ namespace ProjectIndependence.API.Core.Entities.Inventories
             if (quantity > QuantityReserved)
                 throw new InventoryException(InventoryErrors.QuantityExceedsReserve);
 
+            AddMovement(quantity, InventoryMovementType.Release, "Release reserved stock");
+
             QuantityReserved -= quantity;
             UpdatedAt = DateTime.Now;
         }
@@ -68,6 +81,12 @@ namespace ProjectIndependence.API.Core.Entities.Inventories
         {
             if (quantity <= 0)
                 throw new InventoryException(InventoryErrors.InvalidQuantity);
+        }
+
+        private void AddMovement(int quantityChange, InventoryMovementType type, string reference)
+        {
+            var movement = new InventoryMovement(ProductId, quantityChange, type, reference);
+            _movements.Add(movement);
         }
     }
 }
